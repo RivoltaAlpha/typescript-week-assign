262cc532b9ad623cd9d8888ef169bd18f865de98
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-05-21 17:03:42 +0300

    subqueries

--- src/examples/wednesday.ts
+++ src/examples/wednesday.ts
@@ -1,6 +1,6 @@
 // Subqueries are particularly useful for performing complex queries, filtering data, and performing operations that require intermediate calculations
 
-import { executeQuery } from "../config/database"
+import { executeQuery, initializeTables } from "../config/database"
 
 
 
@@ -13,17 +13,36 @@ const subQuery = async (query: string, variable: any[] = []) => {
     }
 }
 
+const transactions = async (query: string, variables: any[] = []) => {
+    try{
+    const res = await executeQuery(query,variables)
+        console.table(res.rows)
+    } catch(error){
+
+    }
+}
+
 
 
 export const Wednesday = async () => {
+        // 1. Create tables if it doesn't exist
+    // await initializeTables();
+
     // SELECT FROM WHERE
-    subQuery('SELECT  *  FROM Employee WHERE department = (SELECT department FROM Departments WHERE deptid=1);')
-    subQuery('SELECT  *  FROM Employee WHERE salary < (SELECT avg(salary) from Employee)')
-    subQuery('SELECT  *  FROM Employee WHERE salary >=(SELECT avg(salary) from Employee);')
-    subQuery('SELECT  *  FROM Employee WHERE department IN (SELECT department FROM Departments);')
-    subQuery('SELECT  *  FROM Employee WHERE department NOT IN (SELECT department FROM Departments);')
+        // subQuery("SELECT  *  FROM TyEmployees WHERE department IN (SELECT department FROM TyEmployees WHERE department= 'IT');")
+        // subQuery("SELECT  *  FROM TyEmployees WHERE department IN (SELECT department FROM TyEmployees WHERE department= 'Sales');")
+    // subQuery('SELECT  *  FROM TyEmployees WHERE salary < (SELECT avg(salary) from TyEmployees)')
+    // subQuery('SELECT  *  FROM TyEmployees WHERE salary <= (SELECT avg(salary) from TyEmployees);')
+    // subQuery('SELECT  *  FROM TyEmployees WHERE department IN (SELECT department FROM TyEmployees);')
+    // subQuery('SELECT  *  FROM TyEmployees WHERE department NOT IN (SELECT department FROM Departments);')
+    subQuery(`
+    SELECT department, salary
+    FROM TyEmployees
+    WHERE department IN ('IT', 'Sales', 'Networking', 'Staff')
+    ORDER BY department ASC, salary DESC;
+`)
    
-   // Multiple
-    subQuery('SELECT empid , name FROM Employee WHERE EXISTS (SELECT 1 FROM Departments WHERE Departments.department = Employee.department);')
-    subQuery('')
+//    // Multiple
+//     subQuery('SELECT empid , name FROM TyEmployees WHERE EXISTS (SELECT 1 FROM Departments WHERE Departments.department = TyEmployees.department);')
+//     subQuery('')
 }
