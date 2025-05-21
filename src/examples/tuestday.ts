8b9381dc386f46522e60a2fd0a4f554f2169b305
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-05-21 11:08:52 +0300

    grouping n expressions

--- src/examples/tuestday.ts
+++ src/examples/tuestday.ts
@@ -25,15 +25,34 @@ export const Tuesday = async () => {
     // Tuesday
     // inner join
     // joinsQuery('SELECT s.first_name, s.last_name, c.course_name FROM enrollments e JOIN students s ON e.student_id = s.student_id JOIN courses c ON e.course_id = c.course_id; ')
+    // left join
+    // joinsQuery('SELECT s.first_name, s.last_name, c.course_name FROM enrollments e LEFT JOIN students s ON e.student_id = s.student_id LEFT JOIN courses c ON e.course_id = c.course_id; ')
+    // right join
+    // joinsQuery('SELECT s.first_name, s.last_name, c.course_name FROM enrollments e RIGHT JOIN students s ON e.student_id = s.student_id RIGHT JOIN courses c ON e.course_id = c.course_id; ')
     // full join
     // joinsQuery('SELECT s.first_name, s.last_name, c.course_name FROM enrollments e FUll OUTER JOIN courses c ON e.course_id = c.course_id; ')
-    joinsQuery('SELECT c.customer_id,c.name,o.order_id,o.total_amount FROM  enrollments c FULL OUTER JOIN orders o ON c.customer_id = o.customer_id WHERE  o.customer_id IS NULL; ')
-
+    // joinsQuery('SELECT c.customer_id,c.name,o.order_id,o.total_amount FROM  enrollments c FULL OUTER JOIN orders o ON c.customer_id = o.customer_id WHERE  o.customer_id IS NULL; ')
 
     // sets
         // filterLectures('SELECT department, COUNT(*) AS total FROM lectures GROUP BY GROUPING SETS (department);')
     // filterLectures('SELECT first_name, COUNT(*) AS total FROM lectures GROUP BY GROUPING SETS (first_name);')
-    filterLectures('SELECT department,first_name, COUNT(*) AS total FROM lectures GROUP BY GROUPING SETS (department,first_name)')
+    // filterLectures('SELECT department,first_name, COUNT(*) AS total FROM lectures GROUP BY GROUPING SETS (department,first_name)')
+
+    // conditional Expressions & Operators
+    // filterLectures('SELECT first_name, last_name, department FROM lectures WHERE department IN (\'It\',\'Computing\')')
+    // filterLectures('SELECT first_name, last_name, department FROM lectures WHERE department NOT IN (\'It\',\'Computing\')')
+    // filterLectures('SELECT first_name, last_name, department FROM lectures WHERE department BETWEEN \'It\' AND \'Computing\'')
+
+    // Grouping
+    // filterLectures('SELECT department, COUNT(*) AS total FROM lectures GROUP BY department HAVING COUNT(*) > 1;')
+    // filterLectures('SELECT department, COUNT(*) AS total FROM lectures GROUP BY department HAVING COUNT(*) > 1 ORDER BY total DESC;')
+    // filterLectures('SELECT department, COUNT(*) AS total FROM lectures GROUP BY department HAVING COUNT(*) > 1 ORDER BY total DESC LIMIT 2;')
+    // filterLectures('SELECT department, COUNT(*) AS total FROM lectures GROUP BY department HAVING COUNT(*) > 1 ORDER BY total DESC OFFSET 2;')
+    // filterLectures('SELECT department, COUNT(*) AS total FROM lectures GROUP BY department HAVING COUNT(*) > 1 ORDER BY total DESC LIMIT 2 OFFSET 2;')
+    filterLectures('SELECT department, COUNT(*) AS total FROM lectures GROUP BY department;')
+
+
+
 
 
 }
